b6505db724cae9243c66c0c52ed78efd8332fb71
Justin Schrader <icd2k3@gmail.com>
2024-02-23 15:07:27 -0800

    chore: file organization

--- generatePDF.ts
+++ generatePDF.ts
@@ -4,7 +4,7 @@ import puppeteer from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`file:${path.join(__dirname, 'dist/index.html')}`);
+  await page.goto(`file:${path.join(__dirname, 'src/index.html')}`);
   await page.pdf({
     path: 'dist/justin-schrader-resume.pdf',
     format: 'A4',
